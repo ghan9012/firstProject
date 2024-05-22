c768a2190421ae13fe06bdffaec36a04b728806b
ghanshyam jadon <ghanshyamjadon62@gmail.com>
2024-05-22 10:39:45 +0530

    i changed front page to new page

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express=require("express");
 const path= require('path');
 const hbs = require('hbs');
 const app=express();
-const port=process.env.PORT||8000;
+const port=process.env.PORT||8006;
 
 const static_path= path.join(__dirname,"../public");
 const templates_path=path.join(__dirname,"../templates");
 const partials_path=path.join(__dirname,"../templates/partials");
+
 
 app.set('view engine', 'hbs');
 app.set('views',templates_path);
